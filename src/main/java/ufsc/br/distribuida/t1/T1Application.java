5b125ff63b8a84ccaa6be628f4741bf629d78ba7
Vitor Hugo Homem Marzarotto <vitormarzarotto@hotmail.com>
2023-10-18 20:07:09 -0300

    Spring Initialzr

---
+++ src/main/java/ufsc/br/distribuida/t1/T1Application.java
@@ -0,0 +1,13 @@
+package ufsc.br.distribuida.t1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class T1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(T1Application.class, args);
+	}
+
+}
---
+++ src/test/java/ufsc/br/distribuida/t1/T1ApplicationTests.java
@@ -0,0 +1,13 @@
+package ufsc.br.distribuida.t1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class T1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
